2259bdb2ade123f1d074ed79033cdbc0104e7eba
owenzheang <s225191589@deakin.edu.au>
2026-05-07 14:38:40 +1000

    Delete tdd-ci-task directory

--- tdd-ci-task/src/main/java/TaskStatusService.java
+++
@@ -1,9 +0,0 @@
-public class TaskStatusService {
-
-    public String getTaskStatus(String studentId, String taskId) {
-        if (studentId.equals("S123") && taskId.equals("9.1P")) {
-            return "Submitted";
-        }
-        return "Not Submitted";
-    }
-}
